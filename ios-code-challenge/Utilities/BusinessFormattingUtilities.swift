e95b04293d0df7c4fdaf367c98250fd19fdde74b
Joey deVilla <joey@joeydevilla.com>
2020-05-04 09:39:48 -0400

    Task 2 - Show business details in details view

---
+++ ios-code-challenge/Utilities/BusinessFormattingUtilities.swift
@@ -0,0 +1,9 @@
+//
+//  BusinessFormattingUtilities.swift
+//  ios-code-challenge
+//
+//  Created by Joey deVilla on 5/4/20.
+//  Copyright © 2020 Dustin Lange. All rights reserved.
+//
+
+import Foundation
